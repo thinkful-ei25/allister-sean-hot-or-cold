00437ee6e6803aa6a3fa4e953ead9e508920eb7b
Allister Rooke <allisterg@gmail.com>
2018-11-08 12:19:49 -0500

    added guess handler

--- src/components/game.js
+++ src/components/game.js
@@ -2,17 +2,37 @@ import React from 'react';
 
 import Header from './header';
 import GuessSection from './guess-section';
-import GuessCount  from './guess-count';
+import GuessCount from './guess-count';
 import GuessList from './guess-list';
 
-export default function Game(props) {
-    return (
-        <div>
-            <Header />
-            <GuessSection feedback="Make your guess!" />
-            <GuessCount count={3} />
-            <GuessList guesses={[10, 15, 25]} />
-        </div>
-    );
-}
+export default class Game extends React.Component {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            count : 3,
+            guesses : [10, 15, 25],
+            curGuess : null,
+            feedback: "Make your guess!"
+        }
+    }
+
+    handleGuess(guess) {
+        this.setState( {
+            curGuess : guess
+        })
+    }
+
+
+    render() {
+
+        return (
+            <div>
+                <Header />
+                <GuessSection feedback={this.state.feedback} handleGuess={(guess) => this.handleGuess(guess)} />
+                <GuessCount count={this.state.count} />
+                <GuessList guesses={this.state.guesses} />
+            </div>
+        );
+    }
+}
--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -3,9 +3,15 @@ import React from 'react';
 import './guess-form.css';
 
 export default function GuessForm(props) {
+    
+    
+    let textInput;
     return (
-        <form>
-            <input type="text" name="userGuess" id="userGuess"
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            props.handleGuess(textInput.value)
+        }}>
+            <input ref={input => textInput = input} type="text" name="userGuess" id="userGuess"
                 className="text" maxLength="3" autoComplete="off"
                 placeholder="Enter your Guess" required />
             <input type="submit" id="guessButton" className="button" name="submit" value="Guess"/>
